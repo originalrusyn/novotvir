5e51c112cf2a4d9aa191b5a59dd245bb5ae4fe81
m <balck.stone.666@gmail.com>
2014-04-27 00:59:11 +0300

    access is denied logic added

--- src/main/java/novotvir/controller/ErrorController.java
+++ src/main/java/novotvir/controller/ErrorController.java
@@ -1,10 +1,12 @@
 package novotvir.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 
+import static org.springframework.http.HttpStatus.FORBIDDEN;
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
@@ -25,5 +27,11 @@ public class ErrorController {
     @ResponseStatus(INTERNAL_SERVER_ERROR)
     public ModelAndView getResourceInternalServerErrorView(){
         return new ModelAndView("internal_server_error");
+    }
+
+    @RequestMapping(value = "/access_is_denied", method = GET)
+    @ResponseStatus(FORBIDDEN)
+    public ModelAndView getAccessDeniedView(){
+        return new ModelAndView("access_is_denied");
     }
 }
