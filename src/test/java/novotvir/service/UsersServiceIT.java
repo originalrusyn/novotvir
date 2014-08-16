9346e63d714a95812f2c94e8247fcb7f7eb8ba14
m <balck.stone.666@gmail.com>
2014-08-16 15:21:54 +0300

    refactoring

--- src/test/java/novotvir/service/UsersServiceIT.java
+++ src/test/java/novotvir/service/UsersServiceIT.java
@@ -2,6 +2,7 @@ package novotvir.service;
 
 import novotvir.persistence.domain.User;
 import novotvir.persistence.repository.UserRepository;
+import novotvir.util.DataBaseIT;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,19 +17,28 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-//@ContextHierarchy({
-//        @ContextConfiguration("/conf/spring/services-test.xml"),
-//        @ContextConfiguration("/conf/spring/dao-test.xml")
-//})
 @TransactionConfiguration
 @ContextConfiguration({"/conf/spring/services-test.xml", "/conf/spring/security.xml", "/conf/spring/dao-test.xml"})
-public class UsersServiceIT {
+public class UsersServiceIT extends DataBaseIT{
 
     @Autowired UsersService usersService;
     @Resource UserRepository userRepository;
 
     @Test
     public void shouldFindUsers() {
+        //given
+        String q = "USER";
+        User user = userRepository.save(new User().setName("name").setEmail("email").setFacebookId("facebookId").setActivationToken("activationToken").setActivated(true).setLastSignInIpAddress("lastSignInIpAddress").setToken("token"));
+
+        //when
+        Set<User> users = usersService.findUsers(q);
+
+        //then
+        assertThat(users.size(), is(0));
+    }
+
+    @Test
+    public void shouldFindUsers2() {
         //given
         String q = "USER";
         User user = userRepository.save(new User().setName("name").setEmail("email").setFacebookId("facebookId").setActivationToken("activationToken").setActivated(true).setLastSignInIpAddress("lastSignInIpAddress").setToken("token"));
