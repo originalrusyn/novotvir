769ea98cad5d3396b94c05709f0b918fce3a0924
Mykhailo Titov <mykhayl.titov@musicqubed.com>
2014-08-28 11:34:59 +0300

    Merge branch 'feature/admin' of https://github.com/originalrusyn/novotvir into feature/admin
    Conflicts:
    	novotvir.iml

--- src/test/java/novotvir/security/service/impl/CustomTokenBasedRememberMeServicesImplTest.groovy
+++ src/test/java/novotvir/security/service/impl/CustomTokenBasedRememberMeServicesImplTest.groovy
@@ -1,9 +1,22 @@
 package novotvir.security.service.impl
 
+import groovy.transform.CompileStatic
+import groovy.transform.TypeChecked
+import org.springframework.security.core.userdetails.UserDetailsService
+import spock.lang.Specification
 
 // @author Titov Mykhaylo (titov) on 14.08.2014.
-class CustomTokenBasedRememberMeServicesImplTest extends spock.lang.Specification {
-    def "MakeTokenSignature"() {
+class CustomTokenBasedRememberMeServicesImplTest extends Specification {
 
+    def userDetailsServiceMock = Mock(UserDetailsService);
+    def customTokenBasedRememberMeServices = new CustomTokenBasedRememberMeServicesImpl("key", userDetailsServiceMock);
+
+    def "should encode userName"() {
+        given:
+        String userName = ":a:b:c"
+        when:
+        def encodedUserName = customTokenBasedRememberMeServices.getEncodedUserName(userName);
+        then:
+        encodedUserName == "|a|b|c"
     }
 }
