535e9b27d358ad523b40bc32e79abcaca18ac922
Mykhailo Titov <mykhayl.titov@musicqubed.com>
2015-04-27 17:18:19 +0300

    1

---
+++ droid/app/src/main/java/novo/tvir/access/GoogleApiService.java
@@ -0,0 +1,11 @@
+package novo.tvir.access;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+import lombok.Delegate;
+
+// @author Titov Mykhaylo (titov) on 27.04.2015.
+public class GoogleApiService {
+
+    @Delegate
+    GoogleApiClient googleApiClient;
+}
